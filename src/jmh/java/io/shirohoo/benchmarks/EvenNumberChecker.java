92c32e917d10b713faaa61fcd9b9a6e56ed31947
shirohoo <shirohoo4102@gmail.com>
2021-10-24 21:55:03 +0900

    홀수짝수 비트연산 성능측정

---
+++ src/jmh/java/io/shirohoo/benchmarks/EvenNumberChecker.java
@@ -0,0 +1,33 @@
+package io.shirohoo.benchmarks;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.infra.Blackhole;
+
+@BenchmarkMode(Mode.Throughput)
+public class EvenNumberChecker {
+
+    @Benchmark
+    public void isEvenNumber(Blackhole bh) {
+        for (int i = 0; i < 100_000_000; i++) {
+            bh.consume(isEvenNumber(i));
+        }
+    }
+
+    public static boolean isEvenNumber(final int number) {
+        return number % 2 == 0;
+    }
+
+    @Benchmark
+    public void isEvenNumberWithBitwiseOperation(Blackhole bh) {
+        for (int i = 0; i < 100_000_000; i++) {
+            bh.consume(isEvenNumberWithBitwiseOperation(i));
+        }
+    }
+
+    public static boolean isEvenNumberWithBitwiseOperation(final int number) {
+        return (number & 1) == 0;
+    }
+
+}
